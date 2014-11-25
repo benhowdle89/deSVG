764d24a8d48f732d70a3068ee967b572fd218eac
Ben Howdle <benhowdle89@gmail.com>
2014-11-25 21:32:47 +0000

    commenting

--- desvg.js
+++ desvg.js
@@ -1,9 +1,11 @@
 (function() {
     var desvg = function(selector, removeinlinecss) {
 
+        // grab all the elements from the document matching the passed in selector
         var images = document.querySelectorAll(selector),
             len = images.length;
 
+        // loops over the matched images
         while (len--) {
             var img = images[len],
                 imgID = img.id,
@@ -15,30 +17,38 @@
                 paths,
                 xhr;
 
+            // set up the AJAX request
             xhr = new XMLHttpRequest();
 
             xhr.open('GET', imgURL, true);
 
             xhr.onload = function() {
+                // get the response in XML format
                 var xml = xhr.responseXML;
 
+                // bail if no XML
                 if (!xml) {
                     return;
                 }
 
+                // this will be the <svg />
                 svg = xml.documentElement;
 
+                // get all the SVG paths
                 paths = svg.querySelectorAll('path');
 
                 if (imgID) {
+                    // re-assign the ID attribute from the <img />
                     svg.id = imgID;
                 }
 
                 if (imgClasses) {
+                    // re-assign the class attribute from the <img />
                     svg.setAttribute('class', imgClasses + ' replaced-svg');
                 }
 
                 if (removeinlinecss) {
+                    // if `removeinlinecss` is true then remove the style attributes from the SVG paths
                     for (var i = 0; i < paths.length; i++) {
                         paths[i].removeAttribute('style');
                     }
@@ -46,7 +56,10 @@
 
                 svg.removeAttribute('xmlns:a');
 
+                // add the new SVG element to the DOM
                 imgParent.appendChild(svg);
+
+                // and remove the original <img />
                 imgParent.removeChild(img);
             };
 
