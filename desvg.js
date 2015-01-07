4eca53fe2a660e44716f55dc85d015ce0fc6482d
Boris Kaiser <mail@boriskaiser.com>
2015-01-07 09:35:07 +0100

    Cleanup

--- desvg.js
+++ desvg.js
@@ -1,73 +1,77 @@
 (function() {
     "use strict";
-    var desvg = function(selector, removeinlinecss) {
 
+    var desvg = function(selector, removeinlinecss) {
         removeinlinecss = removeinlinecss || null;
+
+        // helper function for replacing images with svg
+        var replaceImageWithSVG = function (img) {
+            var imgID = img.id,
+                imgClasses = img.getAttribute('class'),
+                imgURL = img.getAttribute('src'),
+                imgParent = img.parentNode,
+                svg,
+                paths,
+                xhr;
+            // set up the AJAX request
+            xhr = new XMLHttpRequest();
+
+            xhr.open('GET', imgURL, true);
+
+            xhr.onload = function() {
+                // get the response in XML format
+                var xml = xhr.responseXML;
+
+                // bail if no XML
+                if (!xml) {
+                    return;
+                }
+
+                // this will be the <svg />
+                svg = xml.documentElement;
+
+                // get all the SVG paths
+                paths = svg.querySelectorAll('path');
+
+                if (imgID) {
+                    // re-assign the ID attribute from the <img />
+                    svg.id = imgID;
+                }
+
+                if (imgClasses) {
+                    // re-assign the class attribute from the <img />
+                    svg.setAttribute('class', imgClasses + ' replaced-svg');
+                }
+
+                if (removeinlinecss) {
+                    // if `removeinlinecss` is true then remove the style attributes from the SVG paths
+                    for (var i = 0; i < paths.length; i++) {
+                        paths[i].removeAttribute('style');
+                    }
+                }
+
+                svg.removeAttribute('xmlns:a');
+
+                // add the new SVG element to the DOM
+                imgParent.appendChild(svg);
+
+                // and remove the original <img />
+                imgParent.removeChild(img);
+            };
+
+            xhr.send();
+        };
+
 
         // grab all the elements from the document matching the passed in selector
         var images = document.querySelectorAll(selector),
-            len = images.length,
-            replaceImageWithSVG = function (img) {
-                var imgID = img.id,
-                    imgClasses = img.getAttribute('class'),
-                    imgURL = img.getAttribute('src'),
-                    imgParent = img.parentNode,
-                    svg,
-                    paths,
-                    xhr;
-                // set up the AJAX request
-                xhr = new XMLHttpRequest();
-
-                xhr.open('GET', imgURL, true);
-
-                xhr.onload = function() {
-                    // get the response in XML format
-                    var xml = xhr.responseXML;
-
-                    // bail if no XML
-                    if (!xml) {
-                        return;
-                    }
-
-                    // this will be the <svg />
-                    svg = xml.documentElement;
-
-                    // get all the SVG paths
-                    paths = svg.querySelectorAll('path');
-
-                    if (imgID) {
-                        // re-assign the ID attribute from the <img />
-                        svg.id = imgID;
-                    }
-
-                    if (imgClasses) {
-                        // re-assign the class attribute from the <img />
-                        svg.setAttribute('class', imgClasses + ' replaced-svg');
-                    }
-
-                    if (removeinlinecss) {
-                        // if `removeinlinecss` is true then remove the style attributes from the SVG paths
-                        for (var i = 0; i < paths.length; i++) {
-                            paths[i].removeAttribute('style');
-                        }
-                    }
-
-                    svg.removeAttribute('xmlns:a');
-
-                    // add the new SVG element to the DOM
-                    imgParent.appendChild(svg);
-
-                    // and remove the original <img />
-                    imgParent.removeChild(img);
-                };
-
-                xhr.send();
-            };
+            len = images.length;
 
         // loops over the matched images
         while (len--) {
             replaceImageWithSVG(images[len]);
         }
     };
+
     window.deSVG = desvg;
 })();
